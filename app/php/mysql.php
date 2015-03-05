500863b3bff36105bf7894b866a821d7c6800be0
sacratos <pablusko_7@hotmail.com>
2015-03-05 17:33:36 +0100

    base creada, presentacion vista vdoctores.Vista creada. BOWER(Bootstrap,datatables,jquery)

---
+++ app/php/mysql.php
@@ -0,0 +1,6 @@
+<?php
+$gaSql['user'] = "root";
+$gaSql['password'] = "root";
+$gaSql['db'] = "datatables";
+$gaSql['server'] = "localhost";
+?>
---
+++ app/scripts/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $("#miTabla").dataTable({
+    	"paging":   true,
+    	"searching": true,
+        "processing": true,
+        "serverSide": false,
+        'language': {
+               'sProcessing': 'Procesando...',
+               'sLengthMenu': 'Mostrar _MENU_ registros',
+               'sZeroRecords': 'No se encontraron resultados',
+               'sEmptyTable': 'Ningún dato disponible en esta tabla',
+               'sInfo': 'Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros',
+               'sInfoEmpty': 'Mostrando registros del 0 al 0 de un total de 0 registros',
+               'sInfoFiltered': '(filtrado de un total de _MAX_ registros)',
+               'sInfoPostFix': '',
+               'sSearch': 'Buscar:',
+               'sUrl': '',
+               'sInfoThousands': ',',
+               'sLoadingRecords': 'Cargando...',
+               'oPaginate': {
+                   'sFirst': 'Primero',
+                   'sLast': 'Último',
+                   'sNext': 'Siguiente',
+                   'sPrevious': 'Anterior'
+               },
+               'oAria': {
+                   'sSortAscending': ': Activar para ordenar la columna de manera ascendente',
+                   'sSortDescending': ': Activar para ordenar la columna de manera descendente'
+               }
+           },
+           'ajax': 'php/cargar_vdoctores.php'
+
+
+
+
+    }); // FIN DEL DATATABLE
+}); // FIN DEL DOCUMENT READY
